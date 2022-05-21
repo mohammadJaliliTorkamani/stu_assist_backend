3e97f2837a237f19825e8ed44b848a3656416406
mohammad <mohammad.jalili.torkamani@gmail.com>
2022-05-21 14:42:02 +0430

    balance bug fixed (GPA, ECTS), more seurity levels were added

--- services/gpa_calculation.php
+++ services/gpa_calculation.php
@@ -9,64 +9,119 @@ function getToken()
     return trim(substr($val, 7));
 }
 
+function isActiveService($serviceName)
+{
+    $query = "SELECT * FROM Service WHERE name = '$serviceName' AND is_active = '1'";
+    $result = dbQuery($query);
+    return dbNumRows($result) == 1;
+}
+
 function isValid($token)
 {
     if ($token === null || $token === '')
         return false;
 
-    $query = "SELECT * from Token WHERE value = '$token'";
+    $query = "SELECT * from Token WHERE value = '$token' AND is_valid = '1'";
     $result = dbQuery($query);
 
     return dbNumRows($result) > 0;
 }
 
-function getCoupons($token)
+function getBalance($token)
 {
     $query = "SELECT balance FROM Token,User, Wallet 
     WHERE Token.value = '$token' AND Token.user_phone = User.phone AND Wallet.id = User.wallet_id";
     $result = dbQuery($query);
-    $balance = intval(dbFetchAssoc($result)['balance']);
-    return $balance;
+    return intval(dbFetchAssoc($result)['balance']);
 }
 
-function hasEnoughCoupons($coupons)
+function hasEnoughBalance($balance, $serviceCost)
 {
-    return $coupons > 0;
+    return  $balance >= $serviceCost;
 }
 
-function decreaseCoupon($token, $newCoupons)
+function getCost($service_name)
+{
+    $costQuery = "SELECT price FROM Service WHERE name = '$service_name' AND is_active = '1'";
+    $result = dbQuery($costQuery);
+    if (dbNumRows($result) == 1) {
+        return intval(dbFetchAssoc($result)['price']);
+    }
+    return -1;
+}
+
+function getServiceCost($serviceName)
+{
+    $query = "SELECT price FROM Service WHERE name = '$serviceName'";
+    $result = dbQuery($query);
+    return intval(dbFetchAssoc($result)['price']);
+}
+
+function getPhoneNumber($token)
+{
+    $query = "SELECT user_phone FROM Token WHERE value = '$token'";
+    $result = dbQuery($query);
+    return dbFetchAssoc($result)['user_phone'];
+}
+
+function decreaseBalance($token, $newBalance)
 {
     $query = "SELECT Wallet.id FROM Token,User, Wallet 
     WHERE Token.value = '$token' AND Token.user_phone = User.phone AND Wallet.id = User.wallet_id";
     $result = dbQuery($query);
     $walletID = dbFetchAssoc($result)['id'];
-    $query = "UPDATE Wallet SET balance = '$newCoupons' WHERE id = '$walletID'";
+    $query = "UPDATE Wallet SET balance = '$newBalance' WHERE id = '$walletID'";
     return dbQuery($query);
 }
 
+function insertServiceUsage($token)
+{
+    $SERVICE_NAME = 'GPA_Basic_1';
+    $phoneNumber = getPhoneNumber($token);
+    $cost = getCost($SERVICE_NAME);
+    $currentDate = date('Y-m-d');
+    $currentTime = date('H:i:s');
+    $query = "INSERT INTO Service_Usage (service_name, user_phone, paid_cost, date, time) 
+    VALUES ('$SERVICE_NAME','$phoneNumber','$cost','$currentDate','$currentTime')";
+    return dbQuery($query);
+}
+
+$SERVICE_NAME = 'GPA_Basic_1';
 $token = getToken();
+
 if (isValid($token)) {
-    $coupons = getCoupons($token);
-    if (hasEnoughCoupons($coupons)) {
-        $min = $_GET['min'];
-        $max = $_GET['max'];
-        $grade = $_GET['grade'];
-        $gpa = 3 * (($max - $grade) / ($max - $min)) + 1;
-        $result = decreaseCoupon($token, $coupons - 1);
-        if ($result === TRUE) {
-            sendResponseCode();
+    if (isActiveService($SERVICE_NAME)) {
+        $balance = getBalance($token);
+        $serviceCost = getServiceCost($SERVICE_NAME);
+        if (hasEnoughBalance($balance, $serviceCost)) {
+            $min = $_GET['min'];
+            $max = $_GET['max'];
+            $grade = $_GET['grade'];
+            $gpa = 3 * (($max - $grade) / ($max - $min)) + 1;
+            $result1 = decreaseBalance($token, $balance - $serviceCost);
+            $result2 = insertServiceUsage($token);
+
+            if ($result1 === TRUE && $result2 === TRUE) {
+                sendResponseCode();
+                $result = [];
+                $result['error'] = false;
+                $result['data'] = $gpa;
+                $result['message'] = '';
+                echo (json_encode($result));
+            } else
+                sendResponseCode(false);
+        } else {
             $result = [];
-            $result['error'] = false;
-            $result['data'] = $gpa;
-            $result['message'] = '';
+            $result['error'] = true;
+            $result['data'] = null;
+            $result['message'] = 'موجودی ناکافی';
             echo (json_encode($result));
-        } else
-            sendResponseCode(false);
+        }
     } else {
         $result = [];
         $result['error'] = true;
         $result['data'] = null;
-        $result['message'] = 'اتمام کوپن';
+        $result['message'] = 'سرویس غیر فعال یا ناموجود';
         echo (json_encode($result));
     }
 } else
