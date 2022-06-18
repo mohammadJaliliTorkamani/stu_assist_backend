144f309df826f4d0009d492c6cd6436b69dcf9a2
mohammad <mohammad.jalili.torkamani@gmail.com>
2022-06-18 21:51:35 +0430

    translation_office list api

---
+++ services/translation_offices.php
@@ -0,0 +1,36 @@
+<?php
+
+require("../config.php");
+require_once('../user_utils.php');
+
+$token = getToken();
+
+function getLanguages($translationOfficeID)
+{
+    $query = "SELECT persian_equivalent 
+    FROM Translation_Language, Translation_Office_Language 
+    WHERE Translation_Office_Language.language_id  = Translation_Language.id AND Translation_Office_Language.translation_office_id = '$translationOfficeID'";
+    $result = dbQuery($query);
+    $languages = [];
+    while ($row = dbFetchAssoc($result))
+        array_push($languages, $row['persian_equivalent']);
+
+    return $languages;
+}
+if (isValid($token)) {
+    $query = "SELECT id, name, website, phone, postal_address, latitude, longitude FROM Translation_Office WHERE available = '1'";
+    $result = dbQuery($query);
+    $offices = [];
+    $counter = 1;
+    while ($row = dbFetchAssoc($result)) {
+        $office['id'] = $counter++;
+        $office['name'] = $row['name'];
+        $office['website'] = $row['website'];
+        $office['phoneNumber'] = $row['phone'];
+        $office['address'] = ['name' => $row['postal_address'], 'latitude' => $row['latitude'], 'longitude' => $row['longitude']];
+        $office['languages'] = getLanguages(intval($office['id']));
+        array_push($offices, $office);
+    }
+    cook($offices);
+} else
+    cook(null, true, 'invalid token');
