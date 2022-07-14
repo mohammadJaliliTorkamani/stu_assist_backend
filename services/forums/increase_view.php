1b6fc4679c61be6ff505b0012e1a50618684e13f
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-14 10:37:13 +0430

    increase_view was implemented

---
+++ services/forums/increase_view.php
@@ -0,0 +1,13 @@
+<?php
+
+require("../../config.php");
+
+$topicIDs = $_POST['id'];
+
+$query = "UPDATE Topic SET number_of_views = number_of_views + 1 WHERE id = '$topicIDs' AND available = '1'";
+$result = dbQuery($query);
+
+if ($result)
+    cook(null);
+else
+    cook(null, true, 'Error while operating');
