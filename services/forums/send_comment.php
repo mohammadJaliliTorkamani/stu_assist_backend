c77a4ec15dc0cf328a6913038bb851ef3ecd28a6
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-14 15:31:58 +0430

    send comment was implemented

---
+++ services/forums/send_comment.php
@@ -0,0 +1,36 @@
+<?php
+
+require("../../config.php");
+require_once("../../utils/user_utils.php");
+
+$topicID = $_POST['id'];
+$content = $_POST['content'];
+
+function topicExists($topicID)
+{
+    $query = "SELECT id FROM Topic WHERE id = '$topicID' AND available = '1'";
+    $result = dbQuery($query);
+    return dbNumRows($result) > 0;
+}
+
+function createComment($craetorID, $content, $topicID)
+{
+    $currentDate = date('Y-m-d');
+    $currentTime = date('H:i:s');
+    $query = "INSERT INTO Comment(content, creator, topic, creation_date, creation_time) 
+    VALUES ('$content', '$craetorID', '$topicID', '$currentDate', '$currentTime')";
+    return dbQuery($query);
+}
+
+$token = getToken();
+
+if (isValid($token)) {
+    if (topicExists($topicID)) {
+        $craetorID = getUserID($token);
+        createComment($craetorID, $content, $topicID);
+        cook(null);
+    } else {
+        cook(null, true, 'داده های ورودی اشتباه است');
+    }
+} else
+    cook(null, true, 'invalid token');
