4f343ae4e8e8af155ba10462b4d2b9f70f07283b
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-15 11:06:14 +0430

    comments refactored

--- services/forums/comments.php
+++ services/forums/comments.php
@@ -8,14 +8,20 @@ $topicID = $_GET['id'];
 
 $query = "SELECT id, content, creator, creation_date, creation_time 
 FROM Comment 
-WHERE topic = '$topicID' AND available = '1' ORDER BY creation_date, creation_time DESC";
+WHERE topic = '$topicID' AND available = '1' 
+ORDER BY creation_date, creation_time DESC";
+
 $result = dbQuery($query);
 
 while ($row = dbFetchAssoc($result)) {
-    $comment['id'] = (int)$row['id'];
-    $comment['message'] = $row['content'];
-    $comment['creatorID'] = (int)$row['creator'];
-    $comment['commentDateTime'] = $row['creation_date'] . " " . $row['creation_time'];
+
+    $comment = array(
+        'id' => (int)$row['id'],
+        'message' => $row['content'],
+        'creatorID' => (int)$row['creator'],
+        'commentDateTime' => $row['creation_date'] . " " . $row['creation_time']
+    );
+
     array_push($comments, $comment);
 }
 
