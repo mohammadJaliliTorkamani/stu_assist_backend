9b26429c424a3efa4d2ed4aa4131d2331d7e9c59
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-20 11:50:27 +0430

    id was added to blogs_utils (blogs)

--- utils/blog_utils.php
+++ utils/blog_utils.php
@@ -8,7 +8,7 @@ function getPostsOfCategory($categoryID)
     AND BlogPost.id = Relation_BlogPost_BlogCategory.blog_post 
     AND BlogPost.available = '1'";
 
-    $postsQuery = "SELECT BlogPost.title, overview, Photo.path as path, BlogPost.creation_date as CraetionDate, BlogPost.creation_time as CraetionTime 
+    $postsQuery = "SELECT BlogPost.id, BlogPost.title, overview, Photo.path as path, BlogPost.creation_date as CraetionDate, BlogPost.creation_time as CraetionTime 
     FROM BlogPost, Photo 
     WHERE BlogPost.id IN (" . $query . ")";
     $result = dbQuery($postsQuery);
