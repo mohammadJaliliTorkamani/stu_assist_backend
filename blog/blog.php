9d5d663f9675d9f21e3790342dfab5320d1b33de
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-20 14:44:00 +0430

    some spell change in blog

--- blog/blog.php
+++ blog/blog.php
@@ -4,7 +4,7 @@ require("../config.php");
 
 $BlogPostID = $_GET['id'];
 $query = "SELECT BlogPost.title, overview, content, Photo.path as photoPath, 
-BlogPost.creation_date as craetionDate, BlogPost.creation_time as craetionTime 
+BlogPost.creation_date as creationDate, BlogPost.creation_time as creationTime 
 FROM BlogPost, Photo 
 WHERE BlogPost.photo = Photo.id AND BlogPost.id = '$BlogPostID' AND BlogPost.available = '1'";
 
