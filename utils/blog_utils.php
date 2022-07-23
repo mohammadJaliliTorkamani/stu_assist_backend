e45d7b1d340c492d3e12abb02a6d47c14881d9ec
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-23 17:18:50 +0430

    blogs bugs were fixed

--- utils/blog_utils.php
+++ utils/blog_utils.php
@@ -3,14 +3,16 @@
 function getPostsOfCategory($categoryID)
 {
     $query = "SELECT blog_post
-    FROM BlogPost,_BlogPost_BlogCategory_Relation 
-    WHERE blog_category = '$categoryID' 
-    AND BlogPost.id = _BlogPost_BlogCategory_Relation.blog_post 
+    FROM BlogPost, BlogPost_BlogCategory_Relation 
+    WHERE BlogPost_BlogCategory_Relation.blog_category = '$categoryID' 
+    AND BlogPost.id = BlogPost_BlogCategory_Relation.blog_post 
     AND BlogPost.available = '1'";
 
-    $postsQuery = "SELECT BlogPost.id, BlogPost.title, overview, Photo.path as photoPath, BlogPost.creation_date as craetionDate, BlogPost.creation_time as craetionTime 
+    $postsQuery = "SELECT BlogPost.id, BlogPost.title, overview, Photo.path as photoPath, 
+    BlogPost.creation_date as craetionDate, BlogPost.creation_time as craetionTime 
     FROM BlogPost, Photo 
-    WHERE BlogPost.id IN (" . $query . ")";
+    WHERE Photo.id = BlogPost.photo 
+    AND BlogPost.id IN (" . $query . ")";
     $result = dbQuery($postsQuery);
     $posts = [];
 
