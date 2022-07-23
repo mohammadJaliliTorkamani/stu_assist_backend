b775ce4c03fb4a9a8769f4cba088a107b71b6c8a
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-23 14:13:10 +0430

    embassies list was fully implemented

---
+++ services/embassy/embassies.php
@@ -0,0 +1,18 @@
+<?php
+
+require('../../config.php');
+
+$query = "SELECT Embassy.country, Embassy.website, Embassy.phone, Photo.path, 
+Address.state, Address.city, Address.value 
+FROM Embassy, Address, Photo 
+WHERE Embassy.flag_photo = Photo.id 
+AND Address.id = Embassy.address 
+AND Embassy.available = '1'";
+
+$embassies = [];
+$result = dbQuery($query);
+
+while ($row = dbFetchAssoc($result))
+    array_push($embassies, $row);
+
+cook($embassies);
