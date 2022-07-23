8388b23200bb39efdf0f57f9384cc424efc4873b
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-23 14:49:14 +0430

    liked status was added to topic

--- services/forums/topics/topic.php
+++ services/forums/topics/topic.php
@@ -2,6 +2,7 @@
 
 require("../../../config.php");
 require_once('../../../utils/comments_utils.php');
+require_once("../../../utils/user_utils.php");
 
 $topicID = $_GET['topic'];
 
@@ -14,13 +15,30 @@ $numberOfRecords = dbNumRows($result);
 if ($numberOfRecords > 0) {
     $row = dbFetchAssoc($result);
 
-    $topic = array(
-        'id' => (int)$topicID,
-        'name' => $row['name'],
-        'content' => $row['content'],
-        'creatorID' => (int)$row['creator'],
-        'postDateTime' => $row['creation_date'] . " " . $row['creation_time']
-    );
+    $token = getToken();
+    if (!isValid($token))
+        $topic = array(
+            'id' => (int)$topicID,
+            'name' => $row['name'],
+            'content' => $row['content'],
+            'creatorID' => (int)$row['creator'],
+            'liked' => null,
+            'postDateTime' => $row['creation_date'] . " " . $row['creation_time']
+        );
+    else {
+        $userPhone = getPhoneNumber($token);
+        $query = "SELECT id FROM Topic_User_Relation_Like WHERE user_phone = '$userPhone' AND topic_id = '$topicID'";
+        $result = dbQuery($query);
+        $liked = dbNumRows($result) == 1 ? true : false;
+        $topic = array(
+            'id' => (int)$topicID,
+            'name' => $row['name'],
+            'content' => $row['content'],
+            'creatorID' => (int)$row['creator'],
+            'liked' => $liked,
+            'postDateTime' => $row['creation_date'] . " " . $row['creation_time']
+        );
+    }
 }
 
 cook($numberOfRecords > 0 ? $topic : null);
