1970cbdb1574dbbcead03311f663c62d4619e578
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-24 14:06:27 +0430

    embassies name correction

--- services/embassy/embassies.php
+++ services/embassy/embassies.php
@@ -2,7 +2,7 @@
 
 require('../../config.php');
 
-$query = "SELECT Embassy.country, Embassy.website, Embassy.phone, Photo.path, 
+$query = "SELECT Embassy.country as title, Embassy.website as path, Embassy.phone, Photo.path, 
 Address.state, Address.city, Address.value 
 FROM Embassy, Address, Photo 
 WHERE Embassy.flag_photo = Photo.id 
