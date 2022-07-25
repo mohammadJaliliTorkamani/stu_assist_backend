73c4caf9d2bc18432292f50668aedfd1bc17c8d2
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-25 12:42:36 +0430

    tiny profile bug was fixed

--- user/tiny_profile.php
+++ user/tiny_profile.php
@@ -8,7 +8,7 @@ $query = "SELECT name, last_name, profile_photo FROM User WHERE id = '$userID'";
 $result = dbQuery($query);
 $row = dbFetchAssoc($result);
 
-$user = array('fullName' => ($row['name'] ? "" : $row['name']) . " " . ($row['last_name'] === null ? "" : $row['last_name']));
+$user = array('fullName' => ($row['name'] === null ? "" : $row['name']) . " " . ($row['last_name'] === null ? "" : $row['last_name']));
 
 $profilePhotoID = (int)$row['profile_photo'];
 if ($profilePhotoID !== -1) {
