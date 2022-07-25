cd08a0043fe43a1c0dc7cd276d6a2caa21079736
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-25 12:39:39 +0430

    photo was added to tiny_profile

--- user/tiny_profile.php
+++ user/tiny_profile.php
@@ -4,8 +4,19 @@ require("../config.php");
 
 $userID = $_GET['id'];
 
-$query = "SELECT name, last_name FROM User WHERE id = '$userID'";
+$query = "SELECT name, last_name, profile_photo FROM User WHERE id = '$userID'";
 $result = dbQuery($query);
 $row = dbFetchAssoc($result);
 
-cook(array('fullName' => $row['name'] . " " . $row['last_name']));
+$user = array('fullName' => ($row['name'] ? "" : $row['name']) . " " . ($row['last_name'] === null ? "" : $row['last_name']));
+
+$profilePhotoID = (int)$row['profile_photo'];
+if ($profilePhotoID !== -1) {
+    $photoQuery = "SELECT path FROM Photo where id = '$profilePhotoID'";
+    $photoResult = dbQuery($photoQuery);
+    $photo = dbFetchAssoc($photoResult);
+    $user['photo'] = $photo;
+} else
+    $user['photo'] = null;
+
+cook($user);
