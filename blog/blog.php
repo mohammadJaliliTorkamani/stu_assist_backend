7c985974756bbb78bbf6837b05fb20a1552cb6bb
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-26 13:03:50 +0430

    multi request method for blog and topic

--- blog/blog.php
+++ blog/blog.php
@@ -2,13 +2,32 @@
 
 require("../config.php");
 
-$BlogPostID = $_GET['id'];
-$query = "SELECT BlogPost.title, overview, content, Photo.path as photoPath, 
-BlogPost.creation_date as creationDate, BlogPost.creation_time as creationTime 
-FROM BlogPost, Photo 
-WHERE BlogPost.photo = Photo.id AND BlogPost.id = '$BlogPostID' AND BlogPost.available = '1'";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $ceil = !isset($_GET['capacity']) ? 5 : $_GET['capacity'];
 
-$result = dbQuery($query);
-$row = dbFetchAssoc($result);
+    $query = "SELECT BlogPost.title, overview, content, Photo.path as path, 
+    BlogPost.creation_date as CraetionDate, BlogPost.creation_time as CraetionTime 
+    FROM BlogPost, Photo 
+    WHERE BlogPost.photo = Photo.id AND BlogPost.available = '1' 
+    ORDER BY BlogPost.id DESC 
+    LIMIT $ceil";
 
-cook($row);
+    $result = dbQuery($query);
+    $posts = [];
+
+    while ($row = dbFetchAssoc($result))
+        array_push($posts, $row);
+
+    cook($posts);
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $BlogPostID = $_GET['id'];
+    $query = "SELECT BlogPost.title, overview, content, Photo.path as photoPath, 
+    BlogPost.creation_date as creationDate, BlogPost.creation_time as creationTime 
+    FROM BlogPost, Photo 
+    WHERE BlogPost.photo = Photo.id AND BlogPost.id = '$BlogPostID' AND BlogPost.available = '1'";
+
+    $result = dbQuery($query);
+    $row = dbFetchAssoc($result);
+
+    cook($row);
+}
--- services/forums/topics/topic.php
+++ services/forums/topics/topic.php
@@ -4,44 +4,67 @@ require("../../../config.php");
 require_once('../../../utils/comments_utils.php');
 require_once('../../../utils/topics_utils.php');
 require_once("../../../utils/user_utils.php");
+require_once("../../../utils/forums_utils.php");
 
-$topicID = $_GET['topic'];
-
-$query = "SELECT name, content, creator, creation_date, creation_time FROM Topic 
-WHERE id = '$topicID' AND Topic.available = '1'";
-
-$result = dbQuery($query);
-$numberOfRecords = dbNumRows($result);
-
-if ($numberOfRecords > 0) {
-    $row = dbFetchAssoc($result);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $content = $_POST['content'];
+    $category = $_POST['category'];
+    $hallID = $_POST['hall'];
 
     $token = getToken();
-    if (!isValid($token))
-        $topic = array(
-            'id' => (int)$topicID,
-            'name' => $row['name'],
-            'content' => $row['content'],
-            'creatorID' => (int)$row['creator'],
-            'liked' => null,
-            'numberOfLikes' => getNumberOfLikes((int)$topicID),
-            'postDateTime' => $row['creation_date'] . " " . $row['creation_time']
-        );
-    else {
-        $userPhone = getPhoneNumber($token);
-        $query = "SELECT id FROM User_Topic_Relation_Like WHERE user_phone = '$userPhone' AND topic_id = '$topicID'";
-        $result = dbQuery($query);
-        $liked = dbNumRows($result) == 1 ? true : false;
-        $topic = array(
-            'id' => (int)$topicID,
-            'name' => $row['name'],
-            'content' => $row['content'],
-            'creatorID' => (int)$row['creator'],
-            'liked' => $liked,
-            'numberOfLikes' => getNumberOfLikes((int)$topicID),
-            'postDateTime' => $row['creation_date'] . " " . $row['creation_time']
-        );
-    }
-}
 
-cook($numberOfRecords > 0 ? $topic : null);
+    if (isValid($token)) {
+        if (hallExistsInCategory($category, $hallID)) {
+            if (isNewTopic($name)) {
+                $craetorID = getUserID($token);
+                if (hasValidFullName($craetorID)) {
+                    $topicID = createTopic($name, $content, $hallID, $craetorID);
+                    cook($topicID);
+                } else
+                    cook(null, true, 'لطفا ابتدا از حساب کاربری خود نام و نام خانوادگی خود را تکمیل نمایید');
+            } else
+                cook(null, true, 'تاپیک مورد نظر تکراری می باشد');
+        } else
+            cook(null, true, 'داده های ورودی اشتباه است');
+    } else
+        cook(null, true, 'نشست نامعتبر');
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $topicID = $_GET['topic'];
+    $query = "SELECT name, content, creator, creation_date, creation_time FROM Topic WHERE id = '$topicID' AND Topic.available = '1'";
+    $result = dbQuery($query);
+    $numberOfRecords = dbNumRows($result);
+
+    if ($numberOfRecords > 0) {
+        $row = dbFetchAssoc($result);
+
+        $token = getToken();
+        if (!isValid($token))
+            $topic = array(
+                'id' => (int)$topicID,
+                'name' => $row['name'],
+                'content' => $row['content'],
+                'creatorID' => (int)$row['creator'],
+                'liked' => null,
+                'numberOfLikes' => getNumberOfLikes((int)$topicID),
+                'postDateTime' => $row['creation_date'] . " " . $row['creation_time']
+            );
+        else {
+            $userPhone = getPhoneNumber($token);
+            $query = "SELECT id FROM User_Topic_Relation_Like WHERE user_phone = '$userPhone' AND topic_id = '$topicID'";
+            $result = dbQuery($query);
+            $liked = dbNumRows($result) == 1 ? true : false;
+            $topic = array(
+                'id' => (int)$topicID,
+                'name' => $row['name'],
+                'content' => $row['content'],
+                'creatorID' => (int)$row['creator'],
+                'liked' => $liked,
+                'numberOfLikes' => getNumberOfLikes((int)$topicID),
+                'postDateTime' => $row['creation_date'] . " " . $row['creation_time']
+            );
+        }
+    }
+
+    cook($numberOfRecords > 0 ? $topic : null);
+}
