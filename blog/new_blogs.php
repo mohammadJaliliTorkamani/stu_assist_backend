a9b94d21d44c6cc4a3727d6303d7fb1a83fce00f
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-07-30 17:28:30 +0430

    id was added and content was removed from new_blogs

--- blog/new_blogs.php
+++ blog/new_blogs.php
@@ -4,7 +4,7 @@ require("../config.php");
 
 $ceil = !isset($_GET['capacity']) ? 5 : $_GET['capacity'];
 
-$query = "SELECT BlogPost.title, overview, content, Photo.path as path, 
+$query = "SELECT BlogPost.id, BlogPost.title, overview, Photo.path as photoPath, 
 BlogPost.creation_date as CraetionDate, BlogPost.creation_time as CraetionTime 
 FROM BlogPost, Photo 
 WHERE BlogPost.photo = Photo.id AND BlogPost.available = '1' 
