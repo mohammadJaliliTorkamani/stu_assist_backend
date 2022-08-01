da8e9709f40d5deac28700da4ec49dd205a5003e
Mohammad <mohammad.jalili.torkamani@gmail.com>
2022-08-01 12:44:15 +0430

    profile upgraded

--- user/profile.php
+++ user/profile.php
@@ -2,23 +2,28 @@
 
 require("../config.php");
 require_once('../utils/user_utils.php');
+require_once('../utils/address_utils.php');
+require_once('../utils/photo_utils.php');
 
 $token = getToken();
 
 if (isValid($token)) {
     $data = [];
-    $infoQuery = "SELECT balance, wallet_id, name, last_name 
+    $infoQuery = "SELECT balance, wallet_id, name, last_name, profile_photo, address, biography 
     FROM Token, User, Wallet 
     WHERE User.wallet_id = Wallet.id AND Token.value = '$token' AND User.id = Token.user";
 
     $info_result = dbQuery($infoQuery);
     $infoRow = dbFetchAssoc($info_result);
-    $fullName = $infoRow['name'] . ' ' . $infoRow['last_name'];
     $walletID = $infoRow['wallet_id'];
-    $data['fullName'] = $fullName;
+    $data['name'] = $infoRow['name'];
+    $data['lastName'] = $infoRow['last_name'];
+    $data['biography'] = $infoRow['biography'];
     $data['balance'] = intval($infoRow['balance']);
-
-    $query = "SELECT payment_track_id AS ITN, card_number, bank, payment_date, payment_time, order_id 
+    $data['address']=getAddress(intval($infoRow['address']));
+    $data['photoPath']=getPhoto(intval($infoRow['profile_photo']));
+    
+    $query = "SELECT payment_track_id AS ITN, card_number, bank, price, payment_date, payment_time, order_id 
     FROM Charge 
     WHERE wallet_id = '$walletID' AND completed = '1'";
 
@@ -32,6 +37,7 @@ if (isValid($token)) {
             $transcation = array(
                 'id' => $counter++,
                 'issueTrackingNo' => $row['ITN'],
+                'cost' => (int)$row['price'],
                 'orderID' => intval($row['order_id']),
                 'time' => $row['payment_time'],
                 'date' => $row['payment_date']
